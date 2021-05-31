edff98084bf87bf8cd8629434df155035416a618
Hypermnesia <72145776+Hypermnesiaa@users.noreply.github.com>
2021-05-31 15:11:03 -0400

    the first commit in 4 months

--- Hyperplugin/src/me/hypermnesia/hyperplugin/RenameItem.java
+++ Hyperplugin/src/me/hypermnesia/hyperplugin/RenameItem.java
@@ -1,4 +1,4 @@
-package me.hypermnesia.hyperplugin;
+package me.hypermnesia.simpleenchants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,42 +11,46 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class RenameItem implements CommandExecutor {
+public class renameItem implements CommandExecutor {
 
     @SuppressWarnings("deprecation")
-	public boolean onCommand(CommandSender sender,Command cmd,String label,String[] args) {
-        if(label.equalsIgnoreCase("renameitem")) {
+    public boolean onCommand(CommandSender sender,Command cmd,String label,String[] args) {
+        if(label.equalsIgnoreCase("renameitem") || label.equalsIgnoreCase("ri")) {
             if(sender instanceof Player) {
                 Player player = (Player) sender;
-            	List<String> list = new ArrayList<String>();
+                List<String> list = new ArrayList<String>();
                 if (player.hasPermission("renameitem.use")) {
+                    if (args.length == 0) {
+                        player.sendMessage(ChatColor.RED + "Usage: /RENAMEITEM NAME");
+                        return true;
+                    }
                     try {
-                	for (int x = 0; x < args.length; x++) { 	
-                		list.add(args[x]);
-                	}
-                	StringBuilder sb = new StringBuilder();
-                	for (String s : list)
-                	{
-                	    sb.append(s);
-                	    sb.append(" ");
-                	}
-                	ItemStack i = player.getItemInHand();
-                	ItemMeta im = i.getItemMeta();
-                	im.setDisplayName(sb.toString());
-                	i.setItemMeta(im);
-                    player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "The item you were holding has been renamed to " + ChatColor.LIGHT_PURPLE + sb.toString());
-                    return true;
+                        for (int x = 0; x < args.length; x++) {
+                            list.add(args[x]);
+                        }
+                        StringBuilder sb = new StringBuilder();
+                        for (String s : list)
+                        {
+                            sb.append(s);
+                            sb.append(" ");
+                        }
+                        ItemStack i = player.getItemInHand();
+                        ItemMeta im = i.getItemMeta();
+                        im.setDisplayName(ChatColor.translateAlternateColorCodes('&', sb.toString()));
+                        i.setItemMeta(im);
+                        player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "The item you were holding has been renamed to " + ChatColor.translateAlternateColorCodes('&', sb.toString()));
+                        return true;
                     } catch (NullPointerException e) {
                         player.sendMessage(ChatColor.RED + "A item is required in order to rename an item.");
                         return false;
-               
-         
-            }
+
+
+                    }
                 }
                 player.sendMessage(ChatColor.RED + "You have no permission to rename items");
 
 
-                }
+            }
             else{
                 sender.sendMessage(ChatColor.GREEN + "Changing console's name to: args[0]");
 
@@ -57,9 +61,10 @@ public class RenameItem implements CommandExecutor {
         return false;
 
     }
+
     @SuppressWarnings("unused")
-	private final Main plugin;
-    public RenameItem(Main plugin) {
+    private final SimpleEnchants plugin;
+    public renameItem (SimpleEnchants plugin) {
         this.plugin = plugin;
     }
 }
