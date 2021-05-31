002460b51bc83dcb57ee14c6bfbb8b518cf967c2
Hypermnesia <72145776+Hypermnesiaa@users.noreply.github.com>
2021-05-31 15:49:13 -0400

    a small but very important fix

--- Hyperplugin/src/me/hypermnesia/hyperplugin/RenameItem.java
+++ Hyperplugin/src/me/hypermnesia/hyperplugin/RenameItem.java
@@ -34,6 +34,9 @@ public class renameItem implements CommandExecutor {
                             sb.append(s);
                             sb.append(" ");
                         }
+                        if (sb.length() > 0) {
+                            sb.deleteCharAt(sb.length() - 1);
+                        }
                         ItemStack i = player.getItemInHand();
                         ItemMeta im = i.getItemMeta();
                         im.setDisplayName(ChatColor.translateAlternateColorCodes('&', sb.toString()));
